d55f567c147bf23ae5790eb093564a1b19f7abde
Rafael Coelho <rafaellcoellho@gmail.com>
2023-06-14 23:39:20 -0300

    adiciona dominio e repositório de mensagens

---
+++ src/domain/message.py
@@ -0,0 +1,21 @@
+import uuid
+from typing import Optional
+
+from src.domain.user import User
+
+
+class Message:
+    def __init__(
+        self,
+        origin: uuid.UUID,
+        destination: uuid.UUID,
+        content: str,
+        message_id: Optional[uuid.UUID] = None,
+    ):
+        self.id: uuid.UUID = message_id if message_id else uuid.uuid4()
+        self.origin: uuid.UUID = origin
+        self.destination: uuid.UUID = destination
+        self.content: str = content
+
+    def __eq__(self, other):
+        return self.id == other.id
---
+++ src/repository/message.py
@@ -0,0 +1,44 @@
+import abc
+import uuid
+from typing import List, Tuple, Iterator, Optional
+
+from src.domain.message import Message
+from src.libs.tuple_space import AbstractTupleSpace
+
+
+class MessageAbstractRepository(abc.ABC):
+    def __init__(self, tuple_space: AbstractTupleSpace):
+        self._tuple_space: AbstractTupleSpace = tuple_space
+
+    @abc.abstractmethod
+    def add(self, message: Message):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def consume_all_messages_for_user(self, user_id: uuid.UUID) -> Iterator[Message]:
+        raise NotImplementedError
+
+
+class MessageRepository(MessageAbstractRepository):
+    def add(self, message: Message):
+        self._tuple_space.write(
+            (
+                "message",
+                message.id,
+                message.origin,
+                message.destination,
+                message.content,
+            )
+        )
+
+    def consume_all_messages_for_user(self, user_id: uuid.UUID) -> Iterator[Message]:
+        pattern: Tuple = ("message", uuid.UUID, uuid.UUID, user_id, str)
+        while self._tuple_space.read(pattern):
+            message_tuple: Optional[Tuple] = self._tuple_space.take(pattern)
+            if message_tuple:
+                yield Message(
+                    message_id=message_tuple[1],
+                    origin=message_tuple[2],
+                    destination=message_tuple[3],
+                    content=message_tuple[4],
+                )
