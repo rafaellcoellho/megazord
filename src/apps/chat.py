0d9c5fb74ef6068c444ce15c6262fc6f98b791d3
Rafael Coelho <rafaellcoellho@gmail.com>
2023-06-26 01:06:00 -0300

    Implementa espião e checagem de palavras suspeitas (#6)
    * cria espião e seu repositório
    
    * implementa janela para criar/remover palavras suspeitas
    
    * enviar mensagem para o sensor quando existe palavra suspeita

--- src/apps/chat.py
+++ src/apps/chat.py
@@ -1,13 +1,18 @@
 from src.gui.chat import ChatGUI
-from src.libs.tuple_space import TupleSpace
+from src.libs.rpc.objects import Censor
+from src.libs.tuple_space import TupleSpace, AbstractTupleSpace
 from src.repository.message import MessageRepository
+from src.repository.spy import SpyRepository
 from src.repository.user import UserRepository
 
 if __name__ == "__main__":
-    tuple_space = TupleSpace()
+    tuple_space: AbstractTupleSpace = TupleSpace()
     services = {
         "user_repository": UserRepository(tuple_space=tuple_space),
         "message_repository": MessageRepository(tuple_space=tuple_space),
+        "spy_repository": SpyRepository(tuple_space=tuple_space),
+        "censor": Censor.get(),
     }
+
     chat_gui: ChatGUI = ChatGUI(services=services)
     chat_gui.run()
